5bd10ce32d6fd39b74f44a9b46b3a4da063dd946
Jingyun Liang <jingyunliang12@gmail.com>
2021-08-26 16:41:17 +0200

    add clarification of patch_size in main_test_swinir.py

--- main_test_swinir.py
+++ main_test_swinir.py
@@ -17,7 +17,9 @@ def main():
     parser.add_argument('--scale', type=int, default=1, help='scale factor: 1, 2, 3, 4, 8') # 1 for dn and jpeg car
     parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
     parser.add_argument('--jpeg', type=int, default=40, help='scale factor: 10, 20, 30, 40')
-    parser.add_argument('--patch_size', type=int, default=128, help='patch size used in SwinIR')
+    parser.add_argument('--training_patch_size', type=int, default=128, help='patch size used in training SwinIR. '
+                                       'Just used to differentiate two different settings in Table 2 of the paper. '
+                                       'Images are NOT tested patch by patch.')
     parser.add_argument('--large_model', action='store_true', help='use large model, only provided for real image sr')
     parser.add_argument('--model_path', type=str,
                         default='model_zoo/swinir/001_classicalSR_DIV2K_s48w8_SwinIR-M_x2.pth')
@@ -109,7 +111,7 @@ def main():
 def define_model(args):
     # 001 classical image sr
     if args.task == 'classical_sr':
-        model = net(upscale=args.scale, in_chans=3, img_size=args.patch_size, window_size=8,
+        model = net(upscale=args.scale, in_chans=3, img_size=args.training_patch_size, window_size=8,
                     img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
                     mlp_ratio=2, upsampler='pixelshuffle', resi_connection='1conv')
         model.load_state_dict(torch.load(args.model_path)['params'], strict=True)
